8f61eade8fc5a6b61d6ca09c2d2ef11a2df80439
saitoryot <hj-g19008@sist.ac.jp>
2020-09-07 16:03:34 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,79 @@
+//########## ヘッダーファイル読み込み ##########
+#include "DxLib.h"
+
+#define GAME_WIDTH			 960	//画面の横の大きさ
+#define GAME_HEIGHT			 640	//画面の縦の大きさ
+#define GAME_COLOR			  32	//画面のカラービット
+
+#define GAME_WINDOW_BAR		0				//タイトルバーはデフォルトにする
+#define GAME_WINDOW_NAME	"Dxlib_Movie"	//ウィンドウのタイトル
+
+//▼▼▼▼▼ プログラム追加ここから ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+
+//MOVIEフォルダと、mp4ファイルも、追加して下さい
+#define MOVIE_PATH			".\\MOVIE\\neko.mp4"	//動画のパス
+
+int handle = -1;	//動画のハンドル
+
+//▲▲▲▲▲ プログラム追加ここまで ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+
+//########## プログラムで最初に実行される関数 ##########
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	SetOutApplicationLogValidFlag(FALSE);				//log.txtを出力しない
+	ChangeWindowMode(TRUE);								//ウィンドウモードに設定
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//指定の数値でウィンドウを表示する
+	SetWindowStyleMode(GAME_WINDOW_BAR);				//タイトルバーはデフォルトにする
+	SetMainWindowText(TEXT(GAME_WINDOW_NAME));			//ウィンドウのタイトルの文字
+	SetAlwaysRunFlag(TRUE);								//非アクティブでも実行する
+
+	if (DxLib_Init() == -1) { return -1; }	//ＤＸライブラリ初期化処理
+
+	//▼▼▼▼▼ プログラム追加ここから ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+
+	//動画の読み込み
+	handle = LoadGraph(MOVIE_PATH);
+
+	//▲▲▲▲▲ プログラム追加ここまで ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+
+	//無限ループ
+	while (TRUE)
+	{
+		if (ProcessMessage() != 0) { break; }	//メッセージ処理の結果がエラーのとき、強制終了
+		if (ClearDrawScreen() != 0) { break; }	//画面を消去できなかったとき、強制終了
+
+		//▼▼▼▼▼ プログラム追加ここから ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+
+		if (GetMovieStateToGraph(handle) == 0)
+		{
+			SeekMovieToGraph(handle, 0);	//動画の再生バーを最初からにする
+			PlayMovieToGraph(handle);		//動画を再生状態にする
+
+			//動画の音を調整する(0が無音～255が動画の音量)
+			ChangeMovieVolumeToGraph(127, handle);
+		}
+
+		//タイトル動画描画
+		//DrawGraph(0, 0, handle, FALSE);
+
+		//動画をウィンドウサイズと合わせたい！
+		DrawExtendGraph(0, 0, GAME_WIDTH, GAME_HEIGHT, handle, FALSE);
+
+		//▲▲▲▲▲ プログラム追加ここまで ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+
+
+		DrawString(0, 0, "動画を再生しています・・・", GetColor(255, 255, 255));
+
+		ScreenFlip();		//モニタのリフレッシュレートの速さで裏画面を再描画
+	}
+
+	DxLib_End();	//ＤＸライブラリ使用の終了処理
+
+	//▼▼▼▼▼ プログラム追加ここから ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+
+	DeleteGraph(handle);	//動画の削除
+
+	//▲▲▲▲▲ プログラム追加ここまで ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+
+	return 0;
+}
